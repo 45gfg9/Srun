399b58d7c5b95be6fcf72e7953ad8588d1422db5
45gfg9 <45gfg9@45gfg9.net>
2026-06-11 02:01:32 +0800

    compat(mbedtls): update to mbedtls 6 psa crypto api

--- platform/mbedtls.c
+++ platform/mbedtls.c
@@ -6,15 +6,38 @@
 
 #include "compat.h"
 
-#include <mbedtls/md.h>
+#include <psa/crypto.h>
 
 inline size_t sha1_digest(const uint8_t *data, size_t len, uint8_t digest[static 20]) {
-  mbedtls_md(mbedtls_md_info_from_type(MBEDTLS_MD_SHA1), data, len, digest);
-  return 20;
+  size_t hash_len;
+  psa_hash_compute(PSA_ALG_SHA_1, data, len, digest, 20, &hash_len);
+  return hash_len;
 }
 
 inline size_t hmac_md5_digest(const uint8_t *key, size_t key_len, const uint8_t *data, size_t data_len,
                               uint8_t digest[static 16]) {
-  mbedtls_md_hmac(mbedtls_md_info_from_type(MBEDTLS_MD_MD5), key, key_len, data, data_len, digest);
-  return 16;
+  psa_status_t status;
+  psa_key_attributes_t attrs = PSA_KEY_ATTRIBUTES_INIT;
+  psa_key_id_t key_id = PSA_KEY_ID_NULL;
+
+  status = psa_crypto_init();
+  if (status != PSA_SUCCESS) {
+    return 0;
+  }
+
+  psa_set_key_type(&attrs, PSA_KEY_TYPE_HMAC);
+  psa_set_key_bits(&attrs, key_len * 8);
+  psa_set_key_usage_flags(&attrs, PSA_KEY_USAGE_SIGN_MESSAGE);
+  psa_set_key_algorithm(&attrs, PSA_ALG_HMAC(PSA_ALG_MD5));
+
+  status = psa_import_key(&attrs, key, key_len, &key_id);
+  psa_reset_key_attributes(&attrs);
+  if (status != PSA_SUCCESS) {
+    return 0;
+  }
+
+  size_t hash_len;
+  psa_mac_compute(key_id, PSA_ALG_HMAC(PSA_ALG_MD5), data, data_len, digest, 16, &hash_len);
+  psa_destroy_key(key_id);
+  return hash_len;
 }
